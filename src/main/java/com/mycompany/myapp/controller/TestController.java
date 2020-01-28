9db18f38941a5c8fbf60104e92aba77a262273a9
Hyonsong Lee <hynsngl@gmail.com>
2020-01-28 20:11:02 +0900

    Import JSONTest.jsp and JSONTest2.jsp

--- src/main/java/com/mycompany/myapp/controller/TestController.java
+++ src/main/java/com/mycompany/myapp/controller/TestController.java
@@ -25,55 +25,55 @@ public class TestController {
 	public String hello() {
 		return "Hello REST!!";
 	}
-//
-//	@RequestMapping("/member")
-//	public MemberVO member() {
-//		MemberVO vo = new MemberVO();
-//		vo.setId("hong");
-//		vo.setPwd("1234");
-//		vo.setName("홍길동");
-//		vo.setEmail("hong@test.com");
-//		return vo;
-//	}
-//
-//	@RequestMapping("/membersList")
-//	public List<MemberVO> listMembers() {
-//		List<MemberVO> list = new ArrayList<MemberVO>();
-//		for (int i = 0; i < 10; i++) {
-//			MemberVO vo = new MemberVO();
-//			vo.setId("hong" + i);
-//			vo.setPwd("123" + i);
-//			vo.setName("홍길동" + i);
-//			vo.setEmail("hong" + i + "@test.com");
-//			list.add(vo);
-//		}
-//		return list;
-//	}
-//
-//	@RequestMapping("/membersMap")
-//	public Map<Integer, MemberVO> membersMap() {
-//		Map<Integer, MemberVO> map = new HashMap<Integer, MemberVO>();
-//		for (int i = 0; i < 10; i++) {
-//			MemberVO vo = new MemberVO();
-//			vo.setId("hong" + i);
-//			vo.setPwd("123" + i);
-//			vo.setName("홍길동" + i);
-//			vo.setEmail("hong" + i + "@test.com");
-//			map.put(i, vo);
-//		}
-//		return map;
-//	}
-//
-//	@RequestMapping(value = "/notice/{num}", method = RequestMethod.GET)
-//	public int notice(@PathVariable("num") int num) throws Exception {
-//		return num;
-//	}
-//
-//	@RequestMapping(value = "/info", method = RequestMethod.POST)
-//	public void modify(@RequestBody MemberVO vo) {
-//		logger.info(vo.toString());
-//	}
-//
+
+	@RequestMapping("/member")
+	public MemberVO member() {
+		MemberVO vo = new MemberVO();
+		vo.setId("207");
+		vo.setUsername("test333");
+		vo.setPassword("qwer1234");
+		vo.setCreatedAt("2019-11-22T09:30:58.941+0000");
+		return vo;
+	}
+
+	@RequestMapping("/membersList")
+	public List<MemberVO> listMembers() {
+		List<MemberVO> list = new ArrayList<MemberVO>();
+		for (int i = 0; i < 10; i++) {
+			MemberVO vo = new MemberVO();
+			vo.setId("207" + i);
+			vo.setUsername("test333" + i);
+			vo.setPassword("qwer1234" + i);
+			vo.setCreatedAt("2019-11-22 +" + i + " T09:30:58.941+0000");
+			list.add(vo);
+		}
+		return list;
+	}
+
+	@RequestMapping("/membersMap")
+	public Map<Integer, MemberVO> membersMap() {
+		Map<Integer, MemberVO> map = new HashMap<Integer, MemberVO>();
+		for (int i = 0; i < 10; i++) {
+			MemberVO vo = new MemberVO();
+			vo.setId("207" + i);
+			vo.setUsername("test333" + i);
+			vo.setPassword("qwer1234" + i);
+			vo.setCreatedAt("2019-11-22 + " + i + " T09:30:58.941+0000");
+			map.put(i, vo);
+		}
+		return map;
+	}
+
+	@RequestMapping(value = "/notice/{num}", method = RequestMethod.GET)
+	public int notice(@PathVariable("num") int num) throws Exception {
+		return num;
+	}
+
+	@RequestMapping(value = "/info", method = RequestMethod.POST)
+	public void modify(@RequestBody MemberVO vo) {
+		logger.info(vo.toString());
+	}
+
 //	@RequestMapping("/membersList2")
 //	public ResponseEntity<List<MemberVO>> listMembers2() {
 //		List<MemberVO> list = new ArrayList<MemberVO>();
--- src/main/java/com/mycompany/myapp/controller/UserController.java
+++ src/main/java/com/mycompany/myapp/controller/UserController.java
@@ -16,18 +16,24 @@ public class UserController {
 
 	private static final Logger logger = LoggerFactory.getLogger(UserController.class);
 
+//	@RequestMapping(value = "/", method = RequestMethod.GET)
+//	public String home(Locale locale, Model model) {
+//		logger.info("Welcome home! The client locale is {}.", locale);
+//
+//		Date date = new Date();
+//		DateFormat dateFormat = DateFormat.getDateTimeInstance(DateFormat.LONG, DateFormat.LONG, locale);
+//
+//		String formattedDate = dateFormat.format(date);
+//
+//		model.addAttribute("serverTime", formattedDate);
+//
+//		return "index";
+//	}
+	
 	@RequestMapping(value = "/", method = RequestMethod.GET)
 	public String home(Locale locale, Model model) {
-		logger.info("Welcome home! The client locale is {}.", locale);
-
-		Date date = new Date();
-		DateFormat dateFormat = DateFormat.getDateTimeInstance(DateFormat.LONG, DateFormat.LONG, locale);
-
-		String formattedDate = dateFormat.format(date);
-
-		model.addAttribute("serverTime", formattedDate);
-
-		return "index";
+	  return "JSONTest";
 	}
+	
 
 }
