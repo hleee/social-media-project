fd33547fe27e3c57ab7ae623e4a057ae0d26dada
Hyonsong Lee <hynsngl@gmail.com>
2020-01-28 18:57:16 +0900

    Add TestController.java to test REST API

--- src/main/java/com/mycompany/myapp/controller/TestController.java
+++ src/main/java/com/mycompany/myapp/controller/TestController.java
@@ -25,78 +25,78 @@ public class TestController {
 	public String hello() {
 		return "Hello REST!!";
 	}
-
-	@RequestMapping("/member")
-	public MemberVO member() {
-		MemberVO vo = new MemberVO();
-		vo.setId("hong");
-		vo.setPwd("1234");
-		vo.setName("홍길동");
-		vo.setEmail("hong@test.com");
-		return vo;
-	}
-
-	@RequestMapping("/membersList")
-	public List<MemberVO> listMembers() {
-		List<MemberVO> list = new ArrayList<MemberVO>();
-		for (int i = 0; i < 10; i++) {
-			MemberVO vo = new MemberVO();
-			vo.setId("hong" + i);
-			vo.setPwd("123" + i);
-			vo.setName("홍길동" + i);
-			vo.setEmail("hong" + i + "@test.com");
-			list.add(vo);
-		}
-		return list;
-	}
-
-	@RequestMapping("/membersMap")
-	public Map<Integer, MemberVO> membersMap() {
-		Map<Integer, MemberVO> map = new HashMap<Integer, MemberVO>();
-		for (int i = 0; i < 10; i++) {
-			MemberVO vo = new MemberVO();
-			vo.setId("hong" + i);
-			vo.setPwd("123" + i);
-			vo.setName("홍길동" + i);
-			vo.setEmail("hong" + i + "@test.com");
-			map.put(i, vo);
-		}
-		return map;
-	}
-
-	@RequestMapping(value = "/notice/{num}", method = RequestMethod.GET)
-	public int notice(@PathVariable("num") int num) throws Exception {
-		return num;
-	}
-
-	@RequestMapping(value = "/info", method = RequestMethod.POST)
-	public void modify(@RequestBody MemberVO vo) {
-		logger.info(vo.toString());
-	}
-
-	@RequestMapping("/membersList2")
-	public ResponseEntity<List<MemberVO>> listMembers2() {
-		List<MemberVO> list = new ArrayList<MemberVO>();
-		for (int i = 0; i < 10; i++) {
-			MemberVO vo = new MemberVO();
-			vo.setId("lee" + i);
-			vo.setPwd("123" + i);
-			vo.setName("이율곡" + i);
-			vo.setEmail("lee" + i + "@test.com");
-			list.add(vo);
-		}
-		return new ResponseEntity(list, HttpStatus.INTERNAL_SERVER_ERROR);
-	}
-
-	@RequestMapping(value = "/res3")
-	public ResponseEntity res3() {
-		HttpHeaders responseHeaders = new HttpHeaders();
-		responseHeaders.add("Content-Type", "text/html; charset=utf-8");
-		String message = "<script>";
-		message += " alert('새 회원을 등록했습니다.');";
-		message += " location.href='/pro29/test/membersList2'; ";
-		message += " </script>";
-		return new ResponseEntity(message, responseHeaders, HttpStatus.CREATED);
-	}
+//
+//	@RequestMapping("/member")
+//	public MemberVO member() {
+//		MemberVO vo = new MemberVO();
+//		vo.setId("hong");
+//		vo.setPwd("1234");
+//		vo.setName("홍길동");
+//		vo.setEmail("hong@test.com");
+//		return vo;
+//	}
+//
+//	@RequestMapping("/membersList")
+//	public List<MemberVO> listMembers() {
+//		List<MemberVO> list = new ArrayList<MemberVO>();
+//		for (int i = 0; i < 10; i++) {
+//			MemberVO vo = new MemberVO();
+//			vo.setId("hong" + i);
+//			vo.setPwd("123" + i);
+//			vo.setName("홍길동" + i);
+//			vo.setEmail("hong" + i + "@test.com");
+//			list.add(vo);
+//		}
+//		return list;
+//	}
+//
+//	@RequestMapping("/membersMap")
+//	public Map<Integer, MemberVO> membersMap() {
+//		Map<Integer, MemberVO> map = new HashMap<Integer, MemberVO>();
+//		for (int i = 0; i < 10; i++) {
+//			MemberVO vo = new MemberVO();
+//			vo.setId("hong" + i);
+//			vo.setPwd("123" + i);
+//			vo.setName("홍길동" + i);
+//			vo.setEmail("hong" + i + "@test.com");
+//			map.put(i, vo);
+//		}
+//		return map;
+//	}
+//
+//	@RequestMapping(value = "/notice/{num}", method = RequestMethod.GET)
+//	public int notice(@PathVariable("num") int num) throws Exception {
+//		return num;
+//	}
+//
+//	@RequestMapping(value = "/info", method = RequestMethod.POST)
+//	public void modify(@RequestBody MemberVO vo) {
+//		logger.info(vo.toString());
+//	}
+//
+//	@RequestMapping("/membersList2")
+//	public ResponseEntity<List<MemberVO>> listMembers2() {
+//		List<MemberVO> list = new ArrayList<MemberVO>();
+//		for (int i = 0; i < 10; i++) {
+//			MemberVO vo = new MemberVO();
+//			vo.setId("lee" + i);
+//			vo.setPwd("123" + i);
+//			vo.setName("이율곡" + i);
+//			vo.setEmail("lee" + i + "@test.com");
+//			list.add(vo);
+//		}
+//		return new ResponseEntity(list, HttpStatus.INTERNAL_SERVER_ERROR);
+//	}
+//
+//	@RequestMapping(value = "/res3")
+//	public ResponseEntity res3() {
+//		HttpHeaders responseHeaders = new HttpHeaders();
+//		responseHeaders.add("Content-Type", "text/html; charset=utf-8");
+//		String message = "<script>";
+//		message += " alert('새 회원을 등록했습니다.');";
+//		message += " location.href='/pro29/test/membersList2'; ";
+//		message += " </script>";
+//		return new ResponseEntity(message, responseHeaders, HttpStatus.CREATED);
+//	}
 
 }
