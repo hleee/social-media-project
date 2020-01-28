cfeaa27faa5dea7a753ffaf1d81f916b09fb0c50
Hyonsong Lee <hynsngl@gmail.com>
2020-01-28 18:26:26 +0900

    Create MemberVO.java

---
+++ src/main/java/com/mycompany/myapp/controller/MemberVO.java
@@ -0,0 +1,51 @@
+package com.mycompany.myapp.controller;
+
+public class MemberVO {
+	private String id;
+	private String username;
+	private String password;
+	private String createdAt;
+
+	public MemberVO() {
+
+	}
+
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	@Override
+	public String toString() {
+		String info = id + ", " + username + ", " + password + ", " + createdAt;
+		return info;
+	}
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public String getCreatedAt() {
+		return createdAt;
+	}
+
+	public void setCreatedAt(String createdAt) {
+		this.createdAt = createdAt;
+	}
+
+}
