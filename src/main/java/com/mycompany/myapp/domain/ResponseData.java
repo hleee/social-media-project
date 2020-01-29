10bd3948d13dc4ee79f046fdee4fe756f9a8f5e5
Hyonsong Lee <hynsngl@gmail.com>
2020-01-29 20:19:24 +0900

    Update UserDAO

---
+++ src/main/java/com/mycompany/myapp/domain/ResponseData.java
@@ -0,0 +1,7 @@
+package com.mycompany.myapp.domain;
+
+public class ResponseData {
+
+	
+	
+}
--- src/main/java/com/mycompany/myapp/repository/UserDAO.java
+++ src/main/java/com/mycompany/myapp/repository/UserDAO.java
@@ -1,5 +1,7 @@
 package com.mycompany.myapp.repository;
 
+import java.util.List;
+
 import org.apache.ibatis.session.SqlSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataAccessException;
@@ -13,10 +15,29 @@ public class UserDAO {
 	@Autowired
 	private SqlSession sqlSession;
 
+	// 한 명 조회
 	// 매개변수를 지정했으면 selectOne()에서 반복해주어야 (id)
 	public UserVO selectByID(Long id) throws DataAccessException {
 		UserVO userVO = sqlSession.selectOne("mapper.board.selectUser", id);
 		return userVO;
 	}
 
+	// 전체 조회
+	public List<UserVO> selectAllUsers() throws DataAccessException {
+		List<UserVO> userList = null;
+		userList = sqlSession.selectList("mapper.board.selectAllUsers");
+		return userList;
+	}
+
+	// 로그인
+	public UserVO loginByUsername(UserVO userVO) throws DataAccessException {
+		UserVO vo = sqlSession.selectOne("mapper.board.loginByUsername", userVO);
+		return vo;
+	}
+
+	// 회원 가입
+	public int insertUser(UserVO userVO) throws DataAccessException {
+		int res = sqlSession.insert("mapper.board.insertUser", userVO);
+		return res;
+	}
 }
