2f48e847e0408a57b22485b9502e33517aafb16b
Hyonsong Lee <hynsngl@gmail.com>
2020-01-29 12:37:52 +0900

    Create accessingdata directory

--- src/main/java/com/mycompany/myapp/controller/HomeController.java
+++ src/main/java/com/mycompany/myapp/controller/HomeController.java
@@ -1,39 +1,46 @@
 package com.mycompany.myapp.controller;
 
+import java.sql.Connection;
 import java.text.DateFormat;
 import java.util.Date;
 import java.util.Locale;
 
+import javax.activation.DataSource;
+import javax.inject.Inject;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 public class HomeController {
-
+	
 	private static final Logger logger = LoggerFactory.getLogger(HomeController.class);
 
-//	@RequestMapping(value = "/", method = RequestMethod.GET)
-//	public String home(Locale locale, Model model) {
-//		logger.info("Welcome home! The client locale is {}.", locale);
-//
-//		Date date = new Date();
-//		DateFormat dateFormat = DateFormat.getDateTimeInstance(DateFormat.LONG, DateFormat.LONG, locale);
-//
-//		String formattedDate = dateFormat.format(date);
-//
-//		model.addAttribute("serverTime", formattedDate);
-//
-//		return "index";
-//	}
-	
 	@RequestMapping(value = "/", method = RequestMethod.GET)
 	public String home(Locale locale, Model model) {
-	  return "JSONTest2";
+		logger.info("Welcome home! The client locale is {}.", locale);
+
+		Date date = new Date();
+		DateFormat dateFormat = DateFormat.getDateTimeInstance(DateFormat.LONG, DateFormat.LONG, locale);
+
+		String formattedDate = dateFormat.format(date);
+
+		model.addAttribute("serverTime", formattedDate);
+
+		return "index";
+	}
+
+	@RequestMapping(value = "/home", method = RequestMethod.GET)
+	public String home1(Locale locale, Model model) {
+		return "JSONTest";
 	}
 	
-
 }
