04a903a1f79df0983b05a9714b966c5b8434943d
Hyonsong Lee <hynsngl@gmail.com>
2020-02-01 22:25:17 +0900

    Update ControllerToPostOneUser.java

--- src/main/java/com/mycompany/myapp/controller/ControllerToPostOneUser.java
+++ src/main/java/com/mycompany/myapp/controller/ControllerToPostOneUser.java
@@ -11,27 +11,32 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.mycompany.myapp.domain.UserVO;
-import com.mycompany.myapp.service.UserService;
+import com.mycompany.myapp.service.ServiceToInsertOneUser;
 
 @RestController
 @RequestMapping("/*")
 public class ControllerToPostOneUser {
 
-	static Logger logger = LoggerFactory.getLogger(RestAPIController.class);
+	static Logger logger = LoggerFactory.getLogger(ControllerToPostOneUser.class);
 
 	@Autowired
-	UserService userService;
+	ServiceToInsertOneUser serviceToInsertOneUser;
 
-	@RequestMapping(value = "/signup", method = RequestMethod.POST)
-	public ResponseEntity<String> insertUser(@RequestBody UserVO vo) {
+	@Autowired
+	UserVO userVO;
+
+	@RequestMapping(value = "/user", method = RequestMethod.POST)
+	public ResponseEntity<String> insertOneUser(@RequestBody UserVO userVO) {
 		ResponseEntity<String> resEntity = null;
 		try {
 			logger.info("Try loop entered");
-			logger.info(vo.toString());
+			logger.info(userVO.toString());
 			// service의 insert 메서드를 호출
-			int resSignUp = userService.insertUser(vo); // vo 객체를 insert로 전달한 것의 반환값을 resSignUp에 담음
-			logger.info("DB에 추가됐으면 정수 1 반환; resSignUp: " + resSignUp);
+			// vo 객체를 insert로 전달한 것의 반환값을 resSignUp에 담음
+			int integerOneIfInserted = serviceToInsertOneUser.insertOneUser(userVO);
+			logger.info("DB에 추가됐으면 정수 1 반환: " + integerOneIfInserted);
 			resEntity = new ResponseEntity<String>("ADD_SUCCEEDED", HttpStatus.OK);
+			// 응답 객체 출력해보기
 			logger.info("resEntity : " + resEntity);
 		} catch (Exception e) {
 			resEntity = new ResponseEntity<String>(e.getMessage(), HttpStatus.BAD_REQUEST);
