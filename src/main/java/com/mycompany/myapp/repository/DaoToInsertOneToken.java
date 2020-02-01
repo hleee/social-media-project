dc1cecf06c11413745843ace02f2940cd11d57aa
Hyonsong Lee <hynsngl@gmail.com>
2020-02-01 20:54:43 +0900

    Create DaoToInsertOneToken.java

---
+++ src/main/java/com/mycompany/myapp/repository/DaoToInsertOneToken.java
@@ -0,0 +1,25 @@
+package com.mycompany.myapp.repository;
+
+import org.apache.ibatis.session.SqlSession;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataAccessException;
+import org.springframework.stereotype.Repository;
+
+import com.mycompany.myapp.controller.RestAPIController;
+import com.mycompany.myapp.domain.TokenVO;
+
+@Repository
+public class DaoToInsertOneToken {
+
+	static Logger logger = LoggerFactory.getLogger(RestAPIController.class);
+
+	@Autowired
+	private SqlSession sqlSession;
+
+	// 토큰;insert는 int 반환
+	public int insertToken(TokenVO tokenVO) throws DataAccessException {
+		return sqlSession.insert("mapper.userMapper.insertToken", tokenVO);
+	}
+}
