2ee572b18c9680668ec3c05c3545c4d7462cc924
Hyonsong Lee <hynsngl@gmail.com>
2020-02-01 20:35:58 +0900

    Rename display controller

--- src/main/java/com/mycompany/myapp/controller/ControllerToDisplayFtl.java
+++ src/main/java/com/mycompany/myapp/controller/ControllerToDisplayFtl.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
-public class MainController {
+public class ControllerToDisplayFtl {
 
 	@RequestMapping(value = "/signup", method = RequestMethod.GET)
 	public String signup(Locale locale, Model model) {
