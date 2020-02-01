ab9084ae199c9cec7ce6ef41494d5ac5a1c609f3
Hyonsong Lee <hynsngl@gmail.com>
2020-02-01 21:22:44 +0900

    Edit DaoToInsertOneToken.java to clarify the flow

--- src/main/java/com/mycompany/myapp/repository/DaoToInsertOneToken.java
+++ src/main/java/com/mycompany/myapp/repository/DaoToInsertOneToken.java
@@ -18,8 +18,10 @@ public class DaoToInsertOneToken {
 	@Autowired
 	private SqlSession sqlSession;
 
-	// 토큰;insert는 int 반환
-	public int insertToken(TokenVO tokenVO) throws DataAccessException {
-		return sqlSession.insert("mapper.token.insertToken", tokenVO);
+	// insert 메서드는 DB에서 작업한 줄 개수를 정수 형태로 반환
+	// 토큰표에 한 줄을 성공적으로 추가했으면 정수 1 반환
+	public int insertOneToken(TokenVO tokenVO) throws DataAccessException {
+		int integerOneIfInserted = sqlSession.insert("mapper.token.insertOneToken", tokenVO);
+		return integerOneIfInserted;
 	}
 }
