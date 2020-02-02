7c18ef08858e90cac600d3c33481f005d0e3e441
Hyonsong Lee <hynsngl@gmail.com>
2020-02-02 23:31:39 +0900

    Update RestAPIController.java

--- src/main/java/com/mycompany/myapp/controller/RestAPIController.java
+++ src/main/java/com/mycompany/myapp/controller/RestAPIController.java
@@ -64,23 +64,6 @@ public class RestAPIController {
 		return map;
 	}
 
-	// 회원 가입
-	@RequestMapping(value = "/signup", method = RequestMethod.POST)
-	public ResponseEntity<String> insertUser(@RequestBody UserVO vo) {
-		ResponseEntity<String> resEntity = null;
-		try {
-			System.out.println("=====POST method called=====");
-			logger.info("insertUser method called");
-			logger.info(vo.toString());
-			// service의 insert 메서드를 호출
-			int resSignUp = service.insertUser(vo); // vo 객체를 insert로 전달한 것의 반환값을 resSignUp에 담음
-			System.out.println("DB에 추가됐으면 정수 1 반환; resSignUp: " + resSignUp);
-			resEntity = new ResponseEntity<String>("ADD_SUCCEEDED", HttpStatus.OK);
-			System.out.println("resEntity : " + resEntity);
-		} catch (Exception e) {
-			resEntity = new ResponseEntity<String>(e.getMessage(), HttpStatus.BAD_REQUEST);
-		}
-		return resEntity;
 	}
 
 	// 토큰 생성
