593f1aa33622411db6866a598133bb6d36cd0cb2
Hyonsong Lee <hynsngl@gmail.com>
2020-02-03 19:19:57 +0900

    Update PostVO.java

--- src/main/java/com/mycompany/myapp/domain/PostVO.java
+++ src/main/java/com/mycompany/myapp/domain/PostVO.java
@@ -5,10 +5,54 @@ import org.springframework.stereotype.Component;
 @Component
 public class PostVO {
 
-	private int code;
-	private String message;
-	private Object data;
+	private int id;
+	private int userId;
+	private String title;
+	private String content;
+	private String createdAt;
 
-	
-	
+	public PostVO() {
+		super();
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public int getUserId() {
+		return userId;
+	}
+
+	public void setUserId(int userId) {
+		this.userId = userId;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public String getContent() {
+		return content;
+	}
+
+	public void setContent(String content) {
+		this.content = content;
+	}
+
+	public String getCreatedAt() {
+		return createdAt;
+	}
+
+	public void setCreatedAt(String createdAt) {
+		this.createdAt = createdAt;
+	}
+
 }
