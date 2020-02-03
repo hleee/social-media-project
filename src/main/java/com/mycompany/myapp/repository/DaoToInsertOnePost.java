ee5436db25a66fe4e876ad65c5b23adfdc8727b8
Hyonsong Lee <hynsngl@gmail.com>
2020-02-03 19:28:33 +0900

    Create DaoToInsertOnePost.java

---
+++ src/main/java/com/mycompany/myapp/repository/DaoToInsertOnePost.java
@@ -0,0 +1,27 @@
+package com.mycompany.myapp.repository;
+
+import org.apache.ibatis.session.SqlSession;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+import com.mycompany.myapp.domain.PostVO;
+
+@Repository
+public class DaoToInsertOnePost {
+
+	static Logger logger = LoggerFactory.getLogger(DaoToInsertOnePost.class);
+
+	@Autowired
+	private SqlSession sqlSession;
+
+	@Autowired
+	public PostVO postVO;
+
+	public int insertOnePost(PostVO postVO) {
+		int IntegerOneIfInserted = sqlSession.insert("mapper.post.insertOnePost", postVO);
+		return IntegerOneIfInserted;
+	}
+
+}
