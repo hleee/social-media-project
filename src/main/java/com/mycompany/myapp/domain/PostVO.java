7237f7120d3c98c6381734ff6c0d7bda8767d713
Hyonsong Lee <hynsngl@gmail.com>
2020-02-03 19:09:28 +0900

    Create post mapper

---
+++ src/main/java/com/mycompany/myapp/domain/PostVO.java
@@ -0,0 +1,14 @@
+package com.mycompany.myapp.domain;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class PostVO {
+
+	private int code;
+	private String message;
+	private Object data;
+
+	
+	
+}
