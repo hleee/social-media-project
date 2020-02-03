6312fcb962e77fb95c268f245b409ec21caae447
Hyonsong Lee <hynsngl@gmail.com>
2020-02-03 13:00:41 +0900

    Update TokenMaker.java

--- src/main/java/com/mycompany/myapp/controller/TokenMaker.java
+++ src/main/java/com/mycompany/myapp/controller/TokenMaker.java
@@ -2,11 +2,9 @@ package com.mycompany.myapp.controller;
 
 import java.util.Random;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.stereotype.Controller;
 
-@RestController
-@RequestMapping("/*")
+@Controller
 public class TokenMaker {
 
 	public String makeToken() {
