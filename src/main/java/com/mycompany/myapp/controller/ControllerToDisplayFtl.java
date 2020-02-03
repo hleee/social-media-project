ab990017fc31ece5292ebe2e28308e677aecbfc0
Hyonsong Lee <hynsngl@gmail.com>
2020-02-03 13:41:08 +0900

    Update ControllerToDisplayFtl.java

--- src/main/java/com/mycompany/myapp/controller/ControllerToDisplayFtl.java
+++ src/main/java/com/mycompany/myapp/controller/ControllerToDisplayFtl.java
@@ -2,6 +2,8 @@ package com.mycompany.myapp.controller;
 
 import java.util.Locale;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -10,19 +12,24 @@ import org.springframework.web.bind.annotation.RequestMethod;
 @Controller
 public class ControllerToDisplayFtl {
 
+	static Logger logger = LoggerFactory.getLogger(ControllerToDisplayFtl.class);
+
 	@RequestMapping(value = "/signup", method = RequestMethod.GET)
 	public String signup(Locale locale, Model model) {
+		logger.info("signup.ftl called.");
 		return "signup";
 	}
-	
+
 	@RequestMapping(value = "/index", method = RequestMethod.GET)
 	public String index(Locale locale, Model model) {
+		logger.info("index.ftl called.");
 		return "index";
 	}
-	
+
 	@RequestMapping(value = "/login", method = RequestMethod.GET)
 	public String login(Locale locale, Model model) {
+		logger.info("login.ftl called.");
 		return "login";
 	}
-	
+
 }
