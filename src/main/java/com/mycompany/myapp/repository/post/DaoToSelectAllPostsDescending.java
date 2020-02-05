4445bceb92cce72f5da61d2a7767af85fc87f2d4
Hyonsong Lee <hynsngl@gmail.com>
2020-02-05 13:39:43 +0900

    Create DaoToSelectAllPostsDescending.java

---
+++ src/main/java/com/mycompany/myapp/repository/post/DaoToSelectAllPostsDescending.java
@@ -0,0 +1,31 @@
+package com.mycompany.myapp.repository.post;
+
+import java.util.List;
+
+import org.apache.ibatis.session.SqlSession;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+import com.mycompany.myapp.domain.PostVO;
+
+@Repository
+public class DaoToSelectAllPostsDescending {
+
+	static Logger logger = LoggerFactory.getLogger(DaoToSelectAllPostsDescending.class);
+
+	@Autowired
+	public SqlSession sqlSession;
+
+	@Autowired
+	public PostVO postVO;
+
+	public List<PostVO> selectAllPostsDescending() {
+		List<PostVO> allPostsList = null;
+		allPostsList = sqlSession.selectList("mapper.post.selectAllPostsDescending");
+		return allPostsList;
+
+	}
+
+}
