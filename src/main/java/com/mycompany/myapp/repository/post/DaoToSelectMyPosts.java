2533d36d20cb0eab8a24a794d23a916fe067c779
Hyonsong Lee <hynsngl@gmail.com>
2020-02-05 22:03:00 +0900

    Create DaoToSelectMyPosts.java

---
+++ src/main/java/com/mycompany/myapp/repository/post/DaoToSelectMyPosts.java
@@ -0,0 +1,31 @@
+package com.mycompany.myapp.repository.post;
+
+import java.util.List;
+
+import org.apache.ibatis.session.SqlSession;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import com.mycompany.myapp.domain.post.PostVo;
+
+@Component
+public class DaoToSelectMyPosts {
+
+	static Logger logger = LoggerFactory.getLogger(DaoToSelectMyPosts.class);
+
+	@Autowired
+	public SqlSession sqlSession;
+
+	@Autowired
+	public PostVo postVo;
+
+	public List<PostVo> selectMyPosts(long userId) {
+		List<PostVo> myPostsList = null;
+		myPostsList = sqlSession.selectList("mapper.post.selectMyPosts", userId);
+		return myPostsList;
+
+	}
+
+}
