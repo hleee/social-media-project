4398b0f86b34ae3127850c25cc8b316ec7ef1ac4
Hyonsong Lee <hynsngl@gmail.com>
2020-02-06 21:07:05 +0900

    Relocate business logic codes from TokenController to TokenService

--- src/main/java/com/mycompany/myapp/controller/TokenController.java
+++ src/main/java/com/mycompany/myapp/controller/TokenController.java
@@ -14,7 +14,6 @@ import com.mycompany.myapp.domain.TokenVo;
 import com.mycompany.myapp.domain.UserVo;
 import com.mycompany.myapp.service.TokenService;
 import com.mycompany.myapp.service.UserService;
-import com.mycompany.myapp.util.TokenMaker;
 
 @RestController
 @RequestMapping("/*")
@@ -36,35 +35,22 @@ public class TokenController {
 	
 	// 1. 토큰 등록
 	@RequestMapping(value = "/auth", method = RequestMethod.POST)
-	public ResponseVo insertOneToken(@RequestBody UserVo userVoToDb) throws Exception {
+	public ResponseVo insertOneToken(@RequestBody UserVo userVo) throws Exception {
 
-		logger.info("insertOneToken() called to authorise user.");
-
-		// userVO에서 id를 추출
-		UserVo userVoFromDb = userService
-				.selectOneUserByUsernameAndPassword(userVoToDb);
-		long id = userVoFromDb.getId();
-		logger.info("id retrieved from DB: " + id);
-
-		// 토큰 발행
-		String token = TokenMaker.makeToken();
-		logger.info("Token created.");
-
-		// tokenVo에 새로 만든 일련 번호와 DB의 user 표에서 불러온 id 저장
-		tokenVo.setToken(token);
-		tokenVo.setUserId(id);
-		logger.info("tokenVO: token: " + token + ", id: " + id);
-
-		// tokenVo에 담긴 정보를 데이터베이스의 token 표에 넣기
-		tokenService.insertOneToken(tokenVo);
-		logger.info("Token inserted into the database.");
-
+		logger.info("insertOneToken() called.");
+		
+		userVo = userService
+				.selectOneUserByUsernameAndPassword(userVo);
+		logger.info("userVo: " + userVo);
+		
+		tokenVo = tokenService.insertOneToken(userVo);
+		logger.info("tokenVo: " + tokenVo);
+		
 		// responseVo에 code, message, data 설정
 		// data는 토큰 일련 번호를 DB의 토큰 표에 넣고 같이 저장된 userId와 createdAt (토큰 생성 시간)
 		responseVo.setCode(HttpStatus.OK);
 		responseVo.setMessage("Success");
 		responseVo.setData(tokenVo);
-		logger.info("code, message, and data set in responseVo");
 
 		return responseVo;
 
--- src/main/java/com/mycompany/myapp/service/TokenService.java
+++ src/main/java/com/mycompany/myapp/service/TokenService.java
@@ -6,7 +6,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.mycompany.myapp.domain.TokenVo;
+import com.mycompany.myapp.domain.UserVo;
 import com.mycompany.myapp.repository.TokenDao;
+import com.mycompany.myapp.repository.UserDao;
+import com.mycompany.myapp.util.TokenMaker;
 
 @Service
 public class TokenService {
@@ -14,19 +17,42 @@ public class TokenService {
 	static Logger logger = LoggerFactory.getLogger(TokenService.class);
 
 	@Autowired
-	TokenDao tokenDao;
+	public TokenDao tokenDao;
 
 	@Autowired
-	TokenVo tokenVo;
-
+	public TokenVo tokenVo;
+	
+	@Autowired
+	public UserDao userDao;
+	
+	@Autowired
+	public UserVo userVo;
+	
+	// Service에 비즈니스 로직을 구현 --> 컨트롤러와 다오 사이에서 데이터 가공 및 자료형 조율, 조정
 	// 1. 토큰 발급 및 저장
-	public int insertOneToken(TokenVo tokenVo) throws Exception {
+	public TokenVo insertOneToken(UserVo userVoToDb) throws Exception {
 		logger.info("insertOneToken() called.");
 
+		// userVO에서 id를 추출
+		UserVo userVoFromDb = userDao
+				.selectOneUserByUsernameAndPassword(userVoToDb);
+		long id = userVoFromDb.getId();
+		logger.info("id retrieved from DB: " + id);
+
+		// 토큰 발행
+		String token = TokenMaker.makeToken();
+		logger.info("token: " + token);
+
+		// tokenVo에 새로 만든 일련 번호와 DB의 user 표에서 불러온 id 저장
+		tokenVo.setToken(token);
+		tokenVo.setUserId(id);
+		logger.info("tokenVo: " + token);
+
+		// tokenVo에 담긴 정보를 데이터베이스의 token 표에 넣기
 		int integerOneIfInserted = tokenDao.insertOneToken(tokenVo);
 		logger.info("Integer 1 if inserted: " + integerOneIfInserted);
 
-		return integerOneIfInserted;
+		return tokenVo;
 	}
 
 	// 2. 토큰으로 토큰 테이블의 열 정보 조회
