50a8408b5ab71367fe3bc97cb9789327a0325b8c
Hyonsong Lee <hynsngl@gmail.com>
2020-02-06 20:12:19 +0900

    Set logger in UserService to visualise flow

--- src/main/java/com/mycompany/myapp/service/UserService.java
+++ src/main/java/com/mycompany/myapp/service/UserService.java
@@ -24,22 +24,32 @@ public class UserService {
 	// 1. 회원 가입
 	// DB에 성공적으로 추가하면 작업한 줄 개수 반환 (정수 1)
 	public int insertOneUser(UserVo userVo) throws Exception {
+		logger.info("insertOneUser() called.");
+		
 		int integerOneIfInserted = userDao.insertOneUser(userVo);
+		logger.info("Integer 1 if inserted: " + integerOneIfInserted);
+		
 		return integerOneIfInserted;
 	}
 
 	// 2. 전체 회원 조회
 	public List<UserVo> selectAllUsers() throws Exception {
+		logger.info("selectAllUsers() called.");
+		
 		return userDao.selectAllUsers();
 	}
 
 	// 3. ID로 단일 회원 조회
 	public UserVo selectOneUserById(Long id) {
-		return userVo = userDao.selectOneUserByID(id);
+		logger.info("selectOneUserById() called.");
+
+		return userVo = userDao.selectOneUserById(id);
 	}
 
 	// 4. username과 password로 단일 회원 조회
 	public UserVo selectOneUserByUsernameAndPassword(UserVo userVo) {
+		logger.info("selectOneUserByUsernameAndPassword() called.");
+
 		UserVo oneUserByUsernameAndPassword = userDao.selectOneUserByUsernameAndPassword(userVo);
 		return oneUserByUsernameAndPassword;
 	}
