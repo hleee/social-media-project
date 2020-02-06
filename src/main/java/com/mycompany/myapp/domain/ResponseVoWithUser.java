78ca0d7ac7df33ac7b00eac25098372f0928c5e9
Hyonsong Lee <hynsngl@gmail.com>
2020-02-06 21:34:22 +0900

    Create ResponseVoWithUser.java

---
+++ src/main/java/com/mycompany/myapp/domain/ResponseVoWithUser.java
@@ -0,0 +1,58 @@
+package com.mycompany.myapp.domain;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Component;
+
+@Component
+public class ResponseVoWithUser {
+
+	private int code;
+	private String message;
+	private Object data;
+	private Object user;
+
+	public ResponseVoWithUser() {
+
+	}
+
+	public int getCode() {
+		return code;
+	}
+
+	public void setCode(HttpStatus httpStatus) {
+		this.code = httpStatus.value();
+	}
+
+	public String getMessage() {
+		return message;
+	}
+
+	public void setMessage(String message) {
+		this.message = message;
+	}
+
+	public Object getData() {
+		return data;
+	}
+
+	public void setData(Object data) {
+		this.data = data;
+	}
+
+	public Object getUser() {
+		return user;
+	}
+
+	public void setUser(Object user) {
+		this.user = user;
+	}
+
+	// 콘솔창에 출력
+	@Override
+	public String toString() {
+		String info = "ResponseVo: code: " + code + ", message: " 
+			+ message + ", data: " + data + ", user: " + user;
+		return info;
+	}
+
+}
