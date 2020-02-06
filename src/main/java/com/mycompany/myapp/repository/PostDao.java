d975786f53373c1327d9bb64371ce81997303110
Hyonsong Lee <hynsngl@gmail.com>
2020-02-06 19:44:43 +0900

    Update logger in PostService and PostDao

--- src/main/java/com/mycompany/myapp/repository/PostDao.java
+++ src/main/java/com/mycompany/myapp/repository/PostDao.java
@@ -24,42 +24,42 @@ public class PostDao {
 
 	// 1. 글 저장 API
 	public int insertOnePost(PostVo postVo) {
-		logger.info("PostDao: insertOnePost() called.");
+		logger.info("insertOnePost() called.");
 
 		int integerOneIfInserted = sqlSession.insert("mapper.post.insertOnePost", postVo);
-		logger.info("PostDao: Integer 1 if post inserted: " + integerOneIfInserted);
+		logger.info("Integer 1 if post inserted: " + integerOneIfInserted);
 
 		return integerOneIfInserted;
 	}
 
 	// 2. 글 전체 목록 조회 API
 	public List<PostVo> selectAllPostsDescending() {
-		logger.info("PostDao: selectAllPostsDescending() called.");
+		logger.info("selectAllPostsDescending() called.");
 
 		List<PostVo> allPostsList = null;
 		allPostsList = sqlSession.selectList("mapper.post.selectAllPostsDescending");
-		logger.info("PostDao: allPostsList: " + allPostsList);
+		logger.info("allPostsList: " + allPostsList);
 
 		return allPostsList;
 	}
 
 	// 3. 내 글 조회 API
 	public List<PostVo> selectMyPosts(long userId) {
-		logger.info("PostDao: selectMyPosts() called.");
+		logger.info("selectMyPosts() called.");
 
 		List<PostVo> myPostsList = null;
 		myPostsList = sqlSession.selectList("mapper.post.selectMyPosts", userId);
-		logger.info("PostDao: myPostsList: " + myPostsList);
+		logger.info("myPostsList: " + myPostsList);
 
 		return myPostsList;
 	}
 
 	// 4. ID로 글 하나 조회
 	public PostVo selectOnePostById(long id) throws DataAccessException {
-		logger.info("PostDao: selectOnePostById() called.");
+		logger.info("selectOnePostById() called.");
 
 		PostVo postVo = sqlSession.selectOne("mapper.post.selectOnePostById", id);
-		logger.info("PostDao: postVo: " + postVo);
+		logger.info("postVo: " + postVo);
 
 		return postVo;
 	}
--- src/main/java/com/mycompany/myapp/service/PostService.java
+++ src/main/java/com/mycompany/myapp/service/PostService.java
@@ -5,7 +5,6 @@ import java.util.List;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 
 import com.mycompany.myapp.domain.PostVo;
@@ -28,7 +27,7 @@ public class PostService {
 
 	@Autowired
 	public PostVo postVo;
-	
+
 	@Autowired
 	public ResponseVo responseVo;
 
@@ -40,17 +39,17 @@ public class PostService {
 
 	// 1. 글 저장 API
 	public int insertOnePost(PostVo postVo) {
-		logger.info("PostService: insertOnePost() called.");
+		logger.info("insertOnePost() called.");
 
 		int integerOneIfInserted = postDao.insertOnePost(postVo);
-		logger.info("PostService: Integer 1 if post inserted: " + integerOneIfInserted);
+		logger.info("Integer 1 if post inserted: " + integerOneIfInserted);
 
 		return integerOneIfInserted;
 	}
 
 	// 2. 전체 글 목록 조회 API
 	public PostVoWithUser[] selectAllPostsDescending() {
-		logger.info("PostService: selectAllPostsDescending() called.");
+		logger.info("selectAllPostsDescending() called.");
 
 		// 전체 글을 역순으로 담은 목록 객체 생성
 		List<PostVo> allPostsList = postDao.selectAllPostsDescending();
@@ -63,43 +62,44 @@ public class PostService {
 			// allPostsListWithUser에 담음
 			// allPostsListWithUser의 길이까지 1씩 증가
 
-			logger.info("PostService: For loop entered to fill in allPostsListWithUsers.");
+			logger.info("For loop entered to fill in allPostsListWithUsers.");
 
 			// id로 회원 정보를 조회해 가져옴
-			long userIdOfAuthors = allPostsList.get(i).getUserId();
-			logger.info("User id of authors retrieved.");
-			UserVo userVo = userDao.selectOneUserByID(userIdOfAuthors);
-			logger.info("userVo has retrieved user info.");
+			long userIdOfAuthor = allPostsList.get(i).getUserId();
+			logger.info("For loop: user id of author retrieved: " + userIdOfAuthor);
+
+			UserVo userVo = userDao.selectOneUserByID(userIdOfAuthor);
+			logger.info("For loop: userVo: " + userVo);
 
 			// 두 VO 객체로부터 정보를 받아 한 군데로 모을 VO 객체 껍데기 생성
 			PostVoWithUser postVoWithUser = new PostVoWithUser();
 
 			// 회원 정보가 담긴 userVo를 user에 set
 			postVoWithUser.setUser(userVo);
-			logger.info("userVo set to postVoWithUser.");
+			logger.info("For loop: postVoWithUser only with userVo: " + postVoWithUser);
 
 			// 나머지 항목들도 손수 넣어줘야 함
 			// allPostsList에 담긴 i 번째 PostVo 객체에서 getId()
 			// 이후 새로 만든 껍데기인 postVoWithUser에 setId()
 			long id = allPostsList.get(i).getId();
 			postVoWithUser.setId(id);
-			logger.info("id: " + id);
+			logger.info("For loop: id set to postVoWithUser: " + id);
 
 			long userId = allPostsList.get(i).getUserId();
 			postVoWithUser.setUserId(userId);
-			logger.info("userId: " + userId);
+			logger.info("For loop: userId set to postVoWithUser: " + userId);
 
 			String title = allPostsList.get(i).getTitle();
 			postVoWithUser.setTitle(title);
-			logger.info("title: " + title);
+			logger.info("For loop: title set to postVoWithUser: " + title);
 
 			String content = allPostsList.get(i).getContent();
 			postVoWithUser.setContent(content);
-			logger.info("content: " + content);
+			logger.info("For loop: content set to postVoWithUser: " + content);
 
 			String createdAt = allPostsList.get(i).getCreatedAt();
 			postVoWithUser.setCreatedAt(createdAt);
-			logger.info("createdAt: " + createdAt);
+			logger.info("For loop: createdAt set to postVoWithUser: " + createdAt);
 
 			allPostsListWithUsers[i] = postVoWithUser;
 
@@ -123,47 +123,50 @@ public class PostService {
 
 			// id로 회원 정보를 조회해 가져옴
 			UserVo userVo = userDao.selectOneUserByID(userId);
-			logger.info("userVo retrieved with userId: " + userVo);
+			logger.info("For loop: userVo retrieved with userId: " + userVo);
 
 			// 두 VO 객체로부터 정보를 받아 한 군데로 모을 VO 객체 껍데기 생성
 			PostVoWithUser postVoWithUser = new PostVoWithUser();
 
 			// 회원 정보가 담긴 userVo를 user에 set
 			postVoWithUser.setUser(userVo);
-			logger.info("postVoWithUser only with userVo: " + postVoWithUser);
+			logger.info("For loop: postVoWithUser only with userVo: " + postVoWithUser);
 
 			// 나머지 항목들도 손수 넣어줘야 함
 			// allPostsList에 담긴 i 번째 PostVo 객체에서 getId()
 			// 이후 새로 만든 껍데기인 postVoWithUser에 setId()
 			long id = myPostsList.get(i).getId();
 			postVoWithUser.setId(id);
-			logger.info("id set to postVoWithUser: " + id);
+			logger.info("For loop: id set to postVoWithUser: " + id);
 
 			long userId1 = myPostsList.get(i).getUserId();
 			postVoWithUser.setUserId(userId1);
-			logger.info("userId set to postVoWithUser: " + userId1);
+			logger.info("For loop: userId set to postVoWithUser: " + userId1);
 
 			String title = myPostsList.get(i).getTitle();
 			postVoWithUser.setTitle(title);
-			logger.info("title set to postVoWithUser: " + title);
+			logger.info("For loop: title set to postVoWithUser: " + title);
 
 			String content = myPostsList.get(i).getContent();
 			postVoWithUser.setContent(content);
-			logger.info("content set to postVoWithUser: " + content);
+			logger.info("For loop: content set to postVoWithUser: " + content);
 
 			String createdAt = myPostsList.get(i).getCreatedAt();
 			postVoWithUser.setCreatedAt(createdAt);
-			logger.info("createdAt set to postVoWithUser: " + createdAt);
+			logger.info("For loop: createdAt set to postVoWithUser: " + createdAt);
 
 			myPostsListWithUser[i] = postVoWithUser;
 		}
-		
+
 		return myPostsListWithUser;
 
 	}
 
 	// 4. 사용자 ID로 글 하나 조회
 	public PostVo selectOnePostById(long id) {
+
+		logger.info("selectOnePostById() called.");
+
 		return postVo = postDao.selectOnePostById(id);
 	}
 
